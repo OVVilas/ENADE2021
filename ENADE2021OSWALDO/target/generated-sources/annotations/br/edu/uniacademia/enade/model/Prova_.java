c2dca758f054ce055079b3657186d44cf6f79967
Oswaldo <ovargasvilas@yahoo.com.br>
2021-05-24 19:55:50 -0300

    dependencia q tava faltando

--- ENADE2021OSWALDO/target/generated-sources/annotations/br/edu/uniacademia/enade/model/Prova_.java
+++ ENADE2021OSWALDO/target/generated-sources/annotations/br/edu/uniacademia/enade/model/Prova_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-24T13:01:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-24T19:31:47")
 @StaticMetamodel(Prova.class)
 public class Prova_ { 
 
--- ENADE2021OSWALDO/target/generated-sources/annotations/br/edu/uniacademia/enade/model/Questao_.java
+++ ENADE2021OSWALDO/target/generated-sources/annotations/br/edu/uniacademia/enade/model/Questao_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-24T13:01:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-24T19:31:47")
 @StaticMetamodel(Questao.class)
 public class Questao_ { 
 
